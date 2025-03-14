f7ba9780d1fc47cf5177c019c7c35b629c190779
Ravi Sharma <ravisharma@apples-MacBook-Pro-2.local>
2025-03-14 17:21:49 +0530

    update

--- src/register/apiroutes.ts
+++ src/register/apiroutes.ts
@@ -5,7 +5,7 @@ import { UserRoutes } from '../routes';
 
 export const RegisterApiRoutes = (router: Router, prefix: string): void => {
     router.get(prefix, (req: Request, res: Response) => {
-        res.send(`WELCOME MY PROJECT ${nodeEnv.toUpperCase()} ❤ RAVI SHARMA`);
+        res.send(`WELCOME TO MY PROJECT ${nodeEnv.toUpperCase()} ❤`);
     });
 
     router.use(`${prefix}/test`, new UserRoutes().router);
